cb4afe30ac13e989eba9cd244f04b00b773e12f7
Enockwem <wasswaenockmale@gmail.com>
2021-01-02 23:04:05 +0300

    Done with recyclerView, still working on the next activity to display the information about the clicked item

--- Grossary/app/src/main/java/com/example/grossary/GrossaryInfoActivity.kt
+++ Grossary/app/src/main/java/com/example/grossary/GrossaryInfoActivity.kt
@@ -1,11 +1,16 @@
 package com.example.grossary
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class GrossaryInfoActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grossary_info)
+
+        val itemTitle = findViewById<TextView>(R.id.item_title)
+        val itemValue = findViewById<TextView>(R.id.item_value)
+        val itemDescription = findViewById<TextView>(R.id.item_description)
     }
 }
